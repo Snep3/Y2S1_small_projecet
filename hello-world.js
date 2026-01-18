a59979d1b87b0dcaefc6ef696f6df492faec9dda
Snep <gavrielsha359@gmail.com>
2026-01-18 11:28:05 +0200

    hello world file

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.end('Hello World!');
+});
+server.listen(3000, () => {
+  console.log('server runs on http://localhost:3000');
+});
